75b588f231f651d9da6de2407bd804f474b93d22
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2015-09-15 08:01:49 -0400

    Display git commit messages to console

--- GitLogExporter/Program.cs
+++ GitLogExporter/Program.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibGit2Sharp;
 
 namespace GitLogExporter {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Git Log Exporter v1.0.0");
+
+            //C:\Users\nledford\projects\dotnet\ifs\automation
+
+            if (!args.Any()) {
+                Console.WriteLine("Git Repository Path was not provided. Exiting...");
+                Console.ReadKey();
+                return;
+            }
+
+            var path = args.First();
+            Console.WriteLine(path);
+
+            using (var repo = new Repository(path)) {
+                foreach (var commit in repo.Commits) {
+                    Console.WriteLine($"{commit.Message}");
+                }
+            }
 
             Console.ReadKey();
         }
