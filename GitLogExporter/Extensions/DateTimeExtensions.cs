299e8b5e121f67d6a24da8688ca944766a6de7ad
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2015-09-15 08:44:51 -0400

    Add class for DateTime extension methods

---
+++ GitLogExporter/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitLogExporter.Extensions {
+    public static class DateTimeExtensions {
+        public static DateTime Next(this DateTime from,
+                                    DayOfWeek dayOfWeek) {
+            var start = (int) from.DayOfWeek;
+            var target = (int) dayOfWeek;
+            if (target <= start) {
+                target += 7;
+            }
+            return from.AddDays(target - start);
+        }
+
+        public static DateTime Previous(this DateTime from,
+                                        DayOfWeek dayOfWeek) {
+            var start = (int) from.DayOfWeek;
+            var target = (int) dayOfWeek;
+            if (target > start) {
+                target -= 7;
+            }
+            return from.AddDays(target - start);
+        }
+    }
+}
