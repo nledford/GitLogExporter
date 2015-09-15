1d76e835558376f6155bcbbae7808484417fb142
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2015-09-15 10:34:00 -0400

    Group commits by date

--- GitLogExporter/Program.cs
+++ GitLogExporter/Program.cs
@@ -9,7 +9,8 @@ using LibGit2Sharp;
 namespace GitLogExporter {
     public class Program {
         private static readonly StringBuilder Sb = new StringBuilder();
-        private static Commit _last;
+        private static Repository _repo;
+        private static string _divider;
 
         private static void Main(string[] args) {
             Console.WriteLine("Git Log Exporter v1.0.0");
@@ -30,67 +31,58 @@ namespace GitLogExporter {
                           ? DateTime.Now
                           : DateTime.Today.Next(DayOfWeek.Saturday);
 
-            using (var repo = new Repository(path)) {
-                var projectName = repo.Config.Get<string>("core.ProjectName").Value;
+            using (_repo = new Repository(path)) {
+                var projectName = _repo.Config.Get<string>("core.ProjectName").Value;
 
                 Sb.AppendLine(
                     $"Git log for {projectName} from {start.ToShortDateString()} to {end.ToShortDateString()}\n");
 
-                var commits = (from c in repo.Commits
+                var commits = (from c in _repo.Commits
                                where c.Committer.When.DateTime >= start && c.Committer.When.DateTime <= end
                                orderby c.Committer.When.DateTime descending
                                select c);
 
-                _last = commits.Last();
+                BuildCommitDivider(commits);
 
-                var divider = BuildCommitDivider(commits);
-
+                var previousDate = commits.First().Committer.When.DateTime;
+                Sb.AppendLine($"{previousDate.ToString("D")}\n");
                 foreach (var commit in commits) {
-                    var dateAndTime =
-                        $"{commit.Committer.When.DateTime.ToString("D")} - {commit.Committer.When.DateTime.ToString("h:mm:ss tt")}";
-                    var message = $"{commit.Message.Trim()}";
-
-                    Sb.AppendLine(dateAndTime);
-                    Sb.AppendLine(message);
-
-                    if (!commit.Equals(_last)) {
-                        Sb.AppendLine(divider);
+                    if (commit.Committer.When.DateTime.Date != previousDate.Date) {
+                        Sb.AppendLine($"\n{commit.Committer.When.DateTime.ToString("D")}\n");
                     }
+
+                    BuildCommitBlock(Sb, commit);
+
+                    previousDate = commit.Committer.When.DateTime;
                 }
 
                 Console.WriteLine(Sb.ToString());
-            }
-
-            //Console.ReadKey();
+            }            
         }
-        
+
         private static void BuildCommitBlock(StringBuilder sb,
                                              Commit commit) {
-            var dateAndTime =
-                        $"{commit.Committer.When.DateTime.ToString("D")} - {commit.Committer.When.DateTime.ToString("h:mm:ss tt")}";
+            var dateAndTime = $"{commit.Committer.When.DateTime.ToString("h:mm:ss tt")}";
+
+            var author = $"{commit.Committer.Name} <{commit.Committer.Email}>";
             var message = $"{commit.Message.Trim()}";
-
-            sb.AppendLine(dateAndTime);
+            
+            sb.AppendLine($"{dateAndTime} - {author}");
             sb.AppendLine(message);
-
-            if (!commit.Equals(_last)) {
-                sb.AppendLine(divider);
-            }
+            sb.AppendLine(_divider);
         }
 
-        private static string BuildCommitDivider(IEnumerable<Commit> commits) {
+        private static void BuildCommitDivider(IEnumerable<Commit> commits) {
             var dividerLength = (from c in commits
                                  orderby c.Message.Length descending
                                  select c.Message.Length).First();
 
             var length = dividerLength > 80 ? 80 : dividerLength;
 
-            var divider = "";
+            _divider = "";
             for (var i = 0; i < length; i++) {
-                divider += "-";
+                _divider += "-";
             }
-
-            return divider;
         }
     }
 }
