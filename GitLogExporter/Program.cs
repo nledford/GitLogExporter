63b0330d0b75f088d6d0d1b0cf7e398e8c58d608
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2015-09-15 10:38:42 -0400

    Allow path to be entered on program launch

--- GitLogExporter/Program.cs
+++ GitLogExporter/Program.cs
@@ -15,13 +15,16 @@ namespace GitLogExporter {
         private static void Main(string[] args) {
             Console.WriteLine("Git Log Exporter v1.0.0");
 
-            if (!args.Any()) {
+            Console.Write("Enter path of repository to export log: ");
+            var result = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(result)) {
                 Console.WriteLine("Git Repository Path was not provided. Exiting...");
                 Console.ReadKey();
                 return;
             }
 
-            var path = Path.GetFullPath(args.First());
+            var path = Path.GetFullPath(result);
             Console.WriteLine($"Opening repositiory: \"{path}\"...");
 
             var start = DateTime.Now.DayOfWeek == DayOfWeek.Monday
