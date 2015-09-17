cd13d9041be41be277d07da5c2d690e378417977
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2015-09-17 14:15:22 -0400

    Add additional controls to form

--- GitLogExporterGUI/Main.Designer.cs
+++ GitLogExporterGUI/Main.Designer.cs
@@ -33,6 +33,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.dateTo = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.pnlDateRanges.SuspendLayout();
             this.grpDateRanges.SuspendLayout();
             this.SuspendLayout();
@@ -118,18 +120,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(351, 227);
+            this.button1.Location = new System.Drawing.Point(12, 407);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.Size = new System.Drawing.Size(359, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Export Git Log";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(377, 407);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 272);
+            this.textBox1.TabIndex = 6;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 262);
+            this.ClientSize = new System.Drawing.Size(464, 442);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pnlDateRanges);
             this.Controls.Add(this.btnFindPath);
@@ -158,6 +179,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTo;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
