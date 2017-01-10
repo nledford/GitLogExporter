4f7083f48a60ffefee7c43e6a57dc4b679dda378
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2017-01-10 11:41:16 -0500

    ReSharper cleanup

--- GitLogExporterGUI/Extensions/StringExtensions.cs
+++ GitLogExporterGUI/Extensions/StringExtensions.cs
@@ -1,10 +1,11 @@
-﻿namespace GitLogExporterGUI.Extensions {
-    public static class StringExtensions {
-        public static string UppercaseFirst(this string s) {
-            if (string.IsNullOrWhiteSpace(s)) {
-                return string.Empty;
-            }
-            return char.ToUpper(s [0]) + s.Substring(1);
+﻿namespace GitLogExporterGUI.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string UppercaseFirst(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return string.Empty;
+            return char.ToUpper(s[0]) + s.Substring(1);
         }
     }
 }
