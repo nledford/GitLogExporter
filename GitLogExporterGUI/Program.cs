5e533dc2da826841f7af4c6c36c30614eda6df6f
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2017-01-10 11:41:32 -0500

    ReSharper cleanup

--- GitLogExporterGUI/Program.cs
+++ GitLogExporterGUI/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GitLogExporterGUI {
-    static class Program {
+namespace GitLogExporterGUI
+{
+    internal static class Program
+    {
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        private static void Main()
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
