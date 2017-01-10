23740be5c000c1e90f73c15e52b101607ded7c55
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2017-01-10 11:41:01 -0500

    ReSharper cleanup

--- GitLogExporterGUI/Extensions/DateTimeExtensions.cs
+++ GitLogExporterGUI/Extensions/DateTimeExtensions.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GitLogExporterGUI.Extensions {
-    public static class DateTimeExtensions {
+namespace GitLogExporterGUI.Extensions
+{
+    public static class DateTimeExtensions
+    {
         public static DateTime Next(this DateTime from,
-                                    DayOfWeek dayOfWeek) {
+            DayOfWeek dayOfWeek)
+        {
             var start = (int) from.DayOfWeek;
             var target = (int) dayOfWeek;
-            if (target <= start) {
-                target += 7;
-            }
+            if (target <= start) target += 7;
             return from.AddDays(target - start);
         }
 
         public static DateTime Previous(this DateTime from,
-                                        DayOfWeek dayOfWeek) {
+            DayOfWeek dayOfWeek)
+        {
             var start = (int) from.DayOfWeek;
             var target = (int) dayOfWeek;
-            if (target > start) {
-                target -= 7;
-            }
+            if (target > start) target -= 7;
             return from.AddDays(target - start);
         }
 
         public static IEnumerable<DateTime> EachDay(DateTime from,
-                                                    DateTime thru) {
-            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1)) {
-                yield return day;
-            }
-        } 
+            DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1)) yield return day;
+        }
     }
 }
