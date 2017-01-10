090af587ed069037b990955cdf13d344727475a2
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2017-01-10 11:34:14 -0500

    ReSharper cleanup

--- GitLogExporterGUI/Main.cs
+++ GitLogExporterGUI/Main.cs
@@ -6,25 +6,30 @@ using GitLogExporterGUI.Exporters;
 using GitLogExporterGUI.Extensions;
 using LibGit2Sharp;
 
-namespace GitLogExporterGUI {
-    public partial class Main : Form {
+namespace GitLogExporterGUI
+{
+    public partial class Main : Form
+    {
         private const string Version = "v1.1.1";
         private DateTime _end;
         private DateTime _start;
 
-        public Main() {
+        public Main()
+        {
             InitializeComponent();
         }
 
         private string Log { get; set; }
 
-        private string Path {
+        private string Path
+        {
             get { return txtPath.Text; }
             set { txtPath.Text = value; }
         }
 
         private void Main_Load(object sender,
-                               EventArgs e) {
+            EventArgs e)
+        {
             Text += $" - {Version}";
 
             InitializeDates();
@@ -39,38 +44,46 @@ namespace GitLogExporterGUI {
         }
 
         private void btnFindPath_Click(object sender,
-                                       EventArgs e) {
-            using (var dialog = new FolderBrowserDialog()) {
+            EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
                 dialog.ShowNewFolderButton = false;
                 dialog.Description = @"Select the folder of the repository containing the git log you want to export";
                 dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Projects";
-                
+
                 if (dialog.ShowDialog() == DialogResult.OK) Path = dialog.SelectedPath;
             }
         }
 
         private void btnExit_Click(object sender,
-                                   EventArgs e) {
+            EventArgs e)
+        {
             Close();
         }
 
         private void txtPath_TextChanged(object sender,
-                                         EventArgs e) {
+            EventArgs e)
+        {
             btnExportGitLog.Enabled = Path.Length > 0;
         }
 
         private void btnExportGitLog_Click(object sender,
-                                           EventArgs e) {
+            EventArgs e)
+        {
             _start = dateFrom.Value;
             _end = dateTo.Value;
 
             Log = null;
             txtPreviewLog.AppendText($"Generating git log for {Path}, please wait...");
 
-            try {
+            try
+            {
                 var txtExporter = new TxtExporter();
                 Log = txtExporter.ExportGitLog(Path, _start, _end);
-            } catch (RepositoryNotFoundException) {
+            }
+            catch (RepositoryNotFoundException)
+            {
                 MessageBox.Show(
                     @"No git log was found in your selected folder.  Please make sure the folder contains a .git directory and try again.",
                     @"Repository Error",
@@ -80,7 +93,9 @@ namespace GitLogExporterGUI {
                 txtPath.SelectAll();
                 txtPath.Focus();
                 return;
-            } catch (BareRepositoryException) {
+            }
+            catch (BareRepositoryException)
+            {
                 MessageBox.Show(
                     @"The selected folder contains a .git directory, but no commits were found.  Please make sure commits exist in the git log and try again.",
                     @"Bare Repository Error",
@@ -98,43 +113,46 @@ namespace GitLogExporterGUI {
         }
 
         private void btnSaveGitLog_Click(object sender,
-                                         EventArgs e) {
+            EventArgs e)
+        {
             var dialog = new SaveFileDialog {CreatePrompt = false, InitialDirectory = Path};
 
-            if (fmtTxt.Checked) {
-                using (dialog) {
+            if (fmtTxt.Checked)
+                using (dialog)
+                {
                     dialog.FileName +=
                         $"Changes to {TxtExporter.ProjectName} from {_start.ToString("yyyy-MM-dd")} to {_end.ToString("yyyy-MM-dd")}.txt";
                     dialog.Filter = @"Text files (*.txt)|*.txt";
 
                     if (dialog.ShowDialog() == DialogResult.OK) File.WriteAllText(dialog.FileName, Log);
                 }
-            }
-            else {
-                using (dialog) {
+            else
+                using (dialog)
+                {
                     dialog.FileName +=
                         $"Changes to {TxtExporter.ProjectName} from {_start.ToString("yyyy-MM-dd")} to {_end.ToString("yyyy-MM-dd")}.xlsx";
                     dialog.Filter = @"Microsoft Excel files (*.xlsx)|*.xlsx";
 
-                    switch (dialog.ShowDialog()) {
+                    switch (dialog.ShowDialog())
+                    {
                         case DialogResult.OK:
                             var excelExporter = new ExcelExporter();
                             excelExporter.ExportGitLog(Path, dialog.FileName, _start, _end);
                             break;
                     }
                 }
-            }
         }
 
-        private void InitializeDates() {
+        private void InitializeDates()
+        {
             _start = DateTime.Now.DayOfWeek == DayOfWeek.Monday
-                         ? DateTime.Now.Date
-                         : DateTime.Today.Previous(DayOfWeek.Monday);
+                ? DateTime.Now.Date
+                : DateTime.Today.Previous(DayOfWeek.Monday);
 
             _end =
-                (DateTime.Now.DayOfWeek == DayOfWeek.Saturday
-                     ? DateTime.Now.Date
-                     : DateTime.Today.Next(DayOfWeek.Saturday)).AddDays(1).AddTicks(-1);
+            (DateTime.Now.DayOfWeek == DayOfWeek.Saturday
+                ? DateTime.Now.Date
+                : DateTime.Today.Next(DayOfWeek.Saturday)).AddDays(1).AddTicks(-1);
         }
     }
 }
