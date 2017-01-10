ceef675d1e99f52dad31051cac927d59af51c49b
Nathaniel Ledford <nathaniel.ledford@teledyne.com>
2017-01-10 11:38:04 -0500

    ReSharper cleanup'

--- GitLogExporterGUI/Exporters/Commits.cs
+++ GitLogExporterGUI/Exporters/Commits.cs
@@ -5,29 +5,34 @@ using System.Linq;
 using GitLogExporterGUI.Extensions;
 using LibGit2Sharp;
 
-namespace GitLogExporterGUI.Exporters {
-    internal static class Commits {
+namespace GitLogExporterGUI.Exporters
+{
+    internal static class Commits
+    {
         public static string CalculateAverageCommitsPerDay(IEnumerable<Commit> commits,
-                                                           DateTime start,
-                                                           DateTime end) {
+            DateTime start,
+            DateTime end)
+        {
             var commitsPerDay = new List<int>();
             var hasCommits = false;
 
             foreach (
                 var count in
-                    DateTimeExtensions.EachDay(start, end)
-                                      .TakeWhile(day => day.Date <= DateTime.Now.Date)
-                                      .Select(day => commits.Count(c => c.Committer.When.DateTime.Date == day.Date))) {
-                if (count <= 0 && !hasCommits) {}
-                if (count > 0) {
-                    hasCommits = true;
-                    commitsPerDay.Add(count);
+                DateTimeExtensions.EachDay(start, end)
+                    .TakeWhile(day => day.Date <= DateTime.Now.Date)
+                    .Select(day => commits.Count(c => c.Committer.When.DateTime.Date == day.Date)))
+            {
+                if (count <= 0 && !hasCommits)
+                {
                 }
+                if (count <= 0) continue;
+                hasCommits = true;
+                commitsPerDay.Add(count);
             }
 
             return commitsPerDay.Any()
-                       ? Math.Round(commitsPerDay.Average(), 2).ToString(CultureInfo.CurrentCulture)
-                       : "0";
+                ? Math.Round(commitsPerDay.Average(), 2).ToString(CultureInfo.CurrentCulture)
+                : "0";
         }
     }
 }
